95ae6610278d4f501e06022f36c7cd90ac74e935
sirconscious <mehdielbakouri0@gmail.com>
2026-07-31 11:13:48 +0100

    fix : image storage via blob

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    qualities: [75, 80, 85],
+    dangerouslyAllowLocalIP: true,
     remotePatterns: [
       { protocol: "https", hostname: "**" },
       { protocol: "http", hostname: "**" },
